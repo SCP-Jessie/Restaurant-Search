5f788c303a65a9eb170c9a1764e41f359e06ace9
jessie <20jl0358@wwprsd.org>
2020-11-07 00:29:22 -0500

    news-search-update

---
+++ precache-manifest.6d8078c6492b03543b5ac1c82cfe364d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415179c1413e6c1939dd452c11a4ae83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fab947693c0405d49a0c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5c93ca94512d87fc3517",
+    "url": "/static/js/2.3b37abd3.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.3b37abd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab947693c0405d49a0c",
+    "url": "/static/js/main.ee64f5e7.chunk.js"
+  },
+  {
+    "revision": "bd40ac7b411017824616",
+    "url": "/static/js/runtime-main.b2ab8b18.js"
+  }
+]);
